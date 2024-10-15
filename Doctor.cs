d77cc680f4e109d9c83b36c8d079cd3fa0213cc5
andriiholubzxc111 <162205115+andriiholubzxc111@users.noreply.github.com>
2024-10-15 22:44:37 +0300

    Add files via upload

---
+++ Doctor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Doctor
+    {
+        public string LastName { get; set; }
+        public string Specialization { get; set; }
+    }
+
+}
---
+++ Patient.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2
+{
+    public class Patient
+    {
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Diagnosis { get; set; }
+        public string DoctorLastName { get; set; }
+        public string Department { get; set; }
+        public DateTime AdmissionDate { get; set; }
+        public DateTime DischargeDate { get; set; }
+    }
+
+}
